5815bc6c67fad7599ece7034818703cb2e70200d
Victor Cruz <motoggb979@gmail.com>
2022-09-20 18:10:37 -0300

    feat: iniciando Home do site com algumas informações e estilizações

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,7 +1,18 @@
 import { HomeStyled } from "./style";
 
 const Home = () => {
-  return <HomeStyled>hello home</HomeStyled>;
+  return (
+    <HomeStyled>
+      <h2 className="apresentation">
+        <span className="spanApresentation">Olá</span> eu sou o Victor da Cruz
+      </h2>
+      <h2>Desenvolvedor Front-end {"&"} estudante FullStack</h2>
+      <div className="containerButtons">
+        <button className="buttonCV">DOWNLOAD CV</button>
+        <button className="buttonContact">CONTATO</button>
+      </div>
+    </HomeStyled>
+  );
 };
 
 export default Home;
--- components/Home/style.ts
+++ components/Home/style.ts
@@ -1,3 +1,75 @@
 import styled from "styled-components";
 
-export const HomeStyled = styled.div``;
+export const HomeStyled = styled.div`
+  height: 100vh;
+  max-height: 100vh;
+  padding: 78.5px 7% 0 7%;
+
+  .apresentation {
+    .spanApresentation {
+      background: -webkit-linear-gradient(
+        180deg,
+        rgb(var(--color-text)),
+        rgb(var(--color-secondary)) 80%
+      );
+      background-clip: text;
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+    }
+  }
+
+  .containerButtons {
+    display: flex;
+    gap: 30px;
+
+    button {
+      width: 140px;
+      height: 60px;
+      border-radius: 12px;
+
+      font-size: 17px;
+      font-weight: 600;
+      color: rgb(var(--color-text));
+      border: 2px solid rgb(var(--color-text));
+      transition: 300ms;
+
+      &:hover {
+        font-size: 18px;
+        border: 2px solid rgb(var(--color-textMin));
+        color: rgb(var(--color-textMin));
+      }
+    }
+
+    .buttonCV {
+      background: -webkit-linear-gradient(
+        180deg,
+        rgb(var(--color-secondary)) 60%,
+        rgb(var(--color-tertiary))
+      );
+
+      &:hover {
+        background: -webkit-linear-gradient(
+          180deg,
+          rgb(var(--color-tertiary)),
+          rgb(var(--color-secondary)) 60%
+        );
+      }
+    }
+
+    .buttonContact {
+      background: -webkit-linear-gradient(
+        180deg,
+        rgb(var(--color-tertiary)),
+        rgb(var(--color-secondary)) 60%
+      );
+
+      &:hover {
+        background: -webkit-linear-gradient(
+          180deg,
+          rgb(var(--color-secondary)) 60%,
+          rgb(var(--color-tertiary))
+        );
+      }
+    }
+  }
+`;
