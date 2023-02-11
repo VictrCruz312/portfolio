7c4e71cabe459fc1d407c574c4482bbd0b31f84e
VictrCruz312 <motoggb979@gmail.com>
2023-02-11 13:08:08 -0300

    feat: iniciando estrutura do menu projects

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -1,7 +1,80 @@
+import Image from "next/image";
+import Link from "next/link";
 import { ProjectsStyled } from "./style";
 
 const Projects = () => {
-  return <ProjectsStyled>projetos</ProjectsStyled>;
+  return (
+    <ProjectsStyled>
+      <h2 className="projectsTitle">Projects</h2>
+      <ul className="projectsList">
+        <li className="projectsProject">
+          <Link href="http://github.com/VictrCruz312">
+            <a target="_blank">
+              <Image
+                src="/perfil.png"
+                height="400"
+                width="400"
+                alt="teste"
+                objectFit="cover"
+              />
+            </a>
+          </Link>
+        </li>
+        <li className="projectsProject">
+          <Link href="http://github.com/VictrCruz312">
+            <a target="_blank">
+              <Image
+                src="/perfil.png"
+                height="400"
+                width="400"
+                alt="teste"
+                objectFit="cover"
+              />
+            </a>
+          </Link>
+        </li>
+        <li className="projectsProject">
+          <Link href="http://github.com/VictrCruz312">
+            <a target="_blank">
+              <Image
+                src="/perfil.png"
+                height="400"
+                width="400"
+                alt="teste"
+                objectFit="cover"
+              />
+            </a>
+          </Link>
+        </li>
+        <li className="projectsProject">
+          <Link href="http://github.com/VictrCruz312">
+            <a target="_blank">
+              <Image
+                src="/perfil.png"
+                height="400"
+                width="400"
+                alt="teste"
+                objectFit="cover"
+              />
+            </a>
+          </Link>
+        </li>
+        <li className="projectsProject">
+          <Link href="http://github.com/VictrCruz312">
+            <a target="_blank">
+              <Image
+                src="/perfil.png"
+                height="400"
+                width="400"
+                alt="teste"
+                objectFit="cover"
+              />
+            </a>
+          </Link>
+        </li>
+      </ul>
+    </ProjectsStyled>
+  );
 };
 
 export default Projects;
--- components/Projects/style.ts
+++ components/Projects/style.ts
@@ -1,3 +1,19 @@
 import styled from "styled-components";
 
-export const ProjectsStyled = styled.div``;
+export const ProjectsStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 25px;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+
+  .projectsTitle {
+    color: rgb(var(--color-secondary));
+  }
+
+  .projectsList {
+    display: flex;
+  }
+`;
